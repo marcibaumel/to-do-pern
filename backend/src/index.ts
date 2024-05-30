cb304b61c36fb44811fe93de935205ef2a0231f5
Marci <marcibaumel@gmail.com>
2024-05-30 14:31:39 +0200

    validation fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from './data-source';
 import { Routes } from './routes';
 import { port } from './config';
 import * as morgan from 'morgan';
+import { validationResult } from 'express-validator';
 
 function handleError(error, req, res, next) {
     res.status(error.status || 500).send({ message: error.message });
@@ -13,18 +14,26 @@ function handleError(error, req, res, next) {
 AppDataSource.initialize()
     .then(async () => {
         const app = express();
-        app.use(morgan('tiny'))
+        app.use(morgan('tiny'));
         app.use(bodyParser.json());
 
         Routes.forEach((route) => {
-            (app as any)[route.method](route.route, async (req: Request, res: Response, next: Function) => {
-                try {
-                    const result = await new (route.controller as any)()[route.action](req, res, next);
-                    res.json(result);
-                } catch (error) {
-                    next(error);
+            (app as any)[route.method](
+                route.route,
+                ...route.validation,
+                async (req: Request, res: Response, next: Function) => {
+                    try {
+                        const errors = validationResult(req);
+                        if (!errors.isEmpty()) {
+                            return res.status(400).json({ errors: errors.array() });
+                        }
+                        const result = await new (route.controller as any)()[route.action](req, res, next);
+                        res.json(result);
+                    } catch (error) {
+                        next(error);
+                    }
                 }
-            });
+            );
         });
 
         app.use(handleError);
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,3 +1,4 @@
+import { body, param } from 'express-validator';
 import { UserController } from './controller/UserController';
 
 export const Routes = [
@@ -6,29 +7,34 @@ export const Routes = [
         route: '/users',
         controller: UserController,
         action: 'all',
+        validation: []
     },
     {
         method: 'get',
         route: '/users/:id',
         controller: UserController,
         action: 'one',
+        validation: [param('id').isInt()],
     },
     {
         method: 'post',
         route: '/users',
         controller: UserController,
         action: 'save',
+        validation: [body('age').isInt({ min: 0 }).withMessage('Must be positive integer'), body('firstName').isString(), body('lastName').isString()],
     },
     {
         method: 'delete',
         route: '/users/:id',
         controller: UserController,
         action: 'remove',
+        validation: [param('id').isInt()],
     },
     {
         method: 'get',
         route: '/favicon.ico',
         controller: UserController,
         action: 'favicon',
+        validation: []
     },
 ];
